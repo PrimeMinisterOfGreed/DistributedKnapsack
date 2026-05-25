dd8ef95db5f949575332f526137e339c7e8a960f
matteo.ielacqua <matteo.ielacqua@gmail.com>
2026-05-25 23:35:19 +0200

    add serialization methods

--- src/Knapsack/knapsackdpmpi.cpp
+++ src/Knapsack/knapsackdpmpi.cpp
@@ -1,18 +1,36 @@
 #include "knapsack.hpp"
 #include "options_bag.hpp"
 #include <deque>
-#include <Eigen/Dense>
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/vector.hpp>
+#include <vector>
+
 struct NodeTask
 {
 	int startIndex;			   // Starting index of the items assigned to this process
 	int endIndex;			   // Ending index of the items assigned to this process
+
+	template <class Archive>
+	void serialize(Archive &ar, const unsigned int /*version*/)
+	{
+		ar & startIndex;
+		ar & endIndex;
+	}
 };
 
 struct NodeResponse
 {
-    int startIndex;
-    int endIndex;
-    int line[];
+	int startIndex;
+	int endIndex;
+	std::vector<int> line;
+
+	template <class Archive>
+	void serialize(Archive &ar, const unsigned int /*version*/)
+	{
+		ar & startIndex;
+		ar & endIndex;
+		ar & line;
+	}
 };
 
 enum NODETAG
@@ -26,12 +44,10 @@ KnapsackSolution mainnode(boost::mpi::communicator &comm, const std::vector<int>
 						  const std::vector<int> &values, int capacity)
 {
 	int n = weights.size();
-    Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic,Eigen::RowMajor> dp{};
-    dp.resize(n + 1, capacity + 1);
-    dp.fill(0);
-	for (int i = 1; i <= n; ++i)
+    std::vector<std::vector<uint32_t>> dp{};
+ 	for (int i = 1; i <= n; ++i)
 	{
-		boost::mpi::broadcast(comm, dp.row(i-1).data(),capacity  ,0);
+		boost::mpi::broadcast(comm, dp[0],0);
         auto nodes = comm.size() - 1;
         auto chunk_size = get_opts().chunk_size;
         auto tasks = std::deque<NodeTask>(
@@ -56,18 +72,19 @@ KnapsackSolution mainnode(boost::mpi::communicator &comm, const std::vector<int>
             requests.erase(req.second);
         }
 	}
+    return KnapsackSolution{};
 }
 
 void workernode(boost::mpi::communicator &comm, const std::vector<int> &weights, const std::vector<int> &values,
 				int capacity)
 {
 	int n = weights.size();
-    Eigen::Matrix<int, 2, Eigen::Dynamic,Eigen::RowMajor> row{};
-    row.resize(2, capacity + 1);
+    std::vector<uint32_t> line{};
 	for (int i = 1; i <= n; ++i)
 	{
-		boost::mpi::broadcast(comm, row.row(0).data(), capacity + 1, 0);
+		boost::mpi::broadcast(comm, line,0);
         bool end = false;   
+        
 	}
 }
 
