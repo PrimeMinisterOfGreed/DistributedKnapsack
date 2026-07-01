016e8a6b4e92369aed66b57521ae9e584239630b
matteo.ielacqua <matteo.ielacqua@gmail.com>
2026-07-01 16:46:23 +0200

    update conanfile specs

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,9 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeToolchain
-
+from pathlib import Path
 
 class DistKnapsack(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = ["fmt/12.1.0", "spdlog/1.17.0"]
     generators = "CMakeDeps"
 
     def generate(self):
@@ -12,6 +11,14 @@ class DistKnapsack(ConanFile):
         tc.user_presets_path = "build/CMakeUserPresets.json"
         tc.generate()
 
+    def requirements(self):
+        if self.requires is None:
+            return
+        self.requires("fmt/12.1.0")
+        self.requires("spdlog/1.17.0")
+        if not Path("/usr/include/boost").exists():
+            self.requires("boost/1.89.0")
+        pass
     def layout(self):
         cmake_layout(self)
         self.folders.build = "build"
